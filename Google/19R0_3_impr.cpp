f9bde8a0765adb5bab0cd8c35d462be5fb9d969c
skook <kooksk95@snu.ac.kr>
2020-03-30 22:34:56 +0900

    ing

--- Google/19R0_3_impr.cpp
+++ Google/19R0_3_impr.cpp
@@ -12,36 +12,63 @@ typedef vector<long long> vl;
 typedef long long ll;
 typedef pair<int,int> pi;
 
-string min_ab(string a, string b){
-    // do minus
+string add(string a) {
+    int s = a.size();
+    while(s--){
+        if(a[s] == '9') {
+            a[s] = '0';
+            if(s == 0) a.insert(a.begin(), '1');
+        }else {
+            a[s]++;
+            break;
+        }
+    }
+
     return a;
 }
 
-string div_ab(string a, string b){
+bool comp(string a, string b){
+    bool res;
+    // a < b = 1;
+    return res;
+}
+
+string min_ab(string a, string b){
+    int s1 = a.size(), s2 = b.size();
+    bool next = 0;
+    while(s2--){
+        s1--, s2--;
+        if(next) a[s1]--;
+        if(s2 < 0) break;
+        
+        int tmp = a[s1] - b[s2];
+        if(tmp<0) {
+            a[s1] += 10;
+            next = 1;
+        } else next = 0;
+    }
+
+    return a;
+}
+
+string div_ab(string a, string b, int m){
+    // m: 0 = div, 1 = mod
     string cnt = "0";
     while(1){
-        a = min_ab(a, b);
-        // if a<0, then break
-        // cnt ++;
-    }
-
-    return cnt;
-}
-
-string mod_ab(string a, string b){
-    while(1){
         string tmp = min_ab(a, b);
-        // if tmp <0, then break
-        // else a = tmp;
+        if(comp(0, tmp)) break;
+
+        a = tmp;
+        cnt = add(cnt);
     }
 
-    return a;
+    return m ? a : cnt;
 }
 
 string gcd(string a, string b){
     if(b == "0") return a;
     
-    string a_mod_b = mod_ab(a, b);
+    string a_mod_b = div_ab(a, b, 1);
     return gcd(b, a_mod_b);
 }
 
@@ -71,8 +98,9 @@ int main(){
         }
         
         // 1. Get GCD from pos & pos+1
-        raw[pos+1] = gcd(cyp[pos], cyp[pos+1]);
-        raw[pos] = div_ab(cyp[pos], raw[pos+1]);
+        // gcd(a > b)
+        // raw[pos+1] = gcd(cyp[pos], cyp[pos+1]);
+        raw[pos] = div_ab(cyp[pos], raw[pos+1], 0);
         s.insert(raw[pos+1]);
         s.insert(raw[pos]);
         
@@ -82,7 +110,7 @@ int main(){
 
         // ... and raw[pos+2 : L]
         REP(i, pos+2, L){
-            raw[i] = div_ab(cyp[i-1], raw[i-1]);
+            raw[i] = div_ab(cyp[i-1], raw[i-1], 0);
             s.insert(raw[i]);
         }
         
