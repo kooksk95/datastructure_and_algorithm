662f1957d8858e1913578ed4f2178c7461c4783d
skook <kooksk95@snu.ac.kr>
2020-06-06 23:49:51 +0900

    week 3

---
+++ BOJ/intro_study/w3_11053.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX 501
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+#define REP(i,a,b) for(auto i=a; i<=b; i++)
+#define pb push_back
+#define mp make_pair
+#define ENT cout<<"\n";
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vp;
+typedef long long ll;
+typedef pair<int,int> pi;
+
+int dp[MAX+1][MAX+1];
+
+int main(){
+    IOS
+    int n; cin>>n;
+    memset(dp, 0, sizeof(dp));
+
+
+
+    cout << ans;
+
+    return 0;
+}
