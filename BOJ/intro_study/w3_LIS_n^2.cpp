49eb38e654b863cdbf65ff5ccd842d3ff0df075f
skook <kooksk95@snu.ac.kr>
2020-06-09 17:53:27 +0900

    intro week3

--- BOJ/intro_study/w3_LIS_n^2.cpp
+++ BOJ/intro_study/w3_LIS_n^2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define MAX 501
+#define MAX 1000001
 #define IOS ios::sync_with_stdio(0);cin.tie(0);
 #define REP(i,a,b) for(auto i=a; i<=b; i++)
 #define pb push_back
@@ -14,16 +14,21 @@ typedef vector<pair<int, int>> vp;
 typedef long long ll;
 typedef pair<int,int> pi;
 
-int dp[MAX+1][MAX+1];
+int dp[101][100001];
 
 int main(){
     IOS
-    int n; cin>>n;
+    int n, k; cin>>n>>k;
     memset(dp, 0, sizeof(dp));
+    
+    REP(i, 1, n){
+        int w, v; cin >> w >> v;
+        REP(j, 1, k){
+            dp[i][j] = j < w ? dp[i-1][j]
+                        : max(dp[i-1][j], dp[i-1][j-w] + v);
+        }
+    }
 
-
-
-    cout << ans;
-
+    cout << dp[n][k];
     return 0;
 }
