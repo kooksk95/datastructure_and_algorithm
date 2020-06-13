677d31c1f0c1a045c6af1c6750a3fb101182c3c1
skook <kooksk95@snu.ac.kr>
2020-06-13 20:26:35 +0900

    week3 LIS 수정

--- BOJ/intro_study/w3_LIS_n^2.cpp
+++ BOJ/intro_study/w3_LIS_n^2.cpp
@@ -14,21 +14,26 @@ typedef vector<pair<int, int>> vp;
 typedef long long ll;
 typedef pair<int,int> pi;
 
-int dp[101][100001];
+ll arr[MAX], n;
 
 int main(){
     IOS
-    int n, k; cin>>n>>k;
-    memset(dp, 0, sizeof(dp));
-    
-    REP(i, 1, n){
-        int w, v; cin >> w >> v;
-        REP(j, 1, k){
-            dp[i][j] = j < w ? dp[i-1][j]
-                        : max(dp[i-1][j], dp[i-1][j-w] + v);
-        }
-    }
+    cin>>n;
+    REP(i, 1, n) cin >> arr[i];
 
-    cout << dp[n][k];
+    vi len(n+1);
+    int ans = 1;
+
+    REP(k, 1, n){
+        len[k] = 1;
+        REP(i, 1, k-1){
+            if(arr[i] < arr[k]) len[k] = max(len[i]+1, len[k]);
+        }
+        ans = max(len[k], ans);
+    }
+    
+    
+    cout << ans;
+
     return 0;
 }
---
+++ BOJ/intro_study/w3_LIS_nlogn.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX 1000001
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+#define REP(i,a,b) for(auto i=a; i<=b; i++)
+#define pb push_back
+#define mp make_pair
+#define ENT cout<<"\n";
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vp;
+typedef long long ll;
+typedef pair<int,int> pi;
+
+int bests[MAX];
+
+int main(){
+    IOS
+    int n; cin>>n;
+    int ans = 0;
+
+    REP(i, 1, n){
+        int x; cin >> x;
+        int idx = lower_bound(bests, bests+ans+1, x) - bests - 1;
+        int res = idx+1;
+        ans = max(res, ans);
+        bests[res] = x;
+    }
+
+    cout << ans;
+    return 0;
+}
