b9c9ca1a3e7c5ab6036793e24474d7cc1f402d13
skook <kooksk95@snu.ac.kr>
2020-07-11 20:23:24 +0900

    Adv - ing

---
+++ sample.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ENT cout<<"\n";
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+#define MX 10001
+#define REP(i,a,b) for(auto i=a; i<b; i++)
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int x[] = {4,10,20};
+int y[] = {1,6,20};
+
+double get_dist(int i, int j){
+	return (pow(x[i]-x[j],2) + pow(y[i]-y[j],2));
+}
+
+int main(int argc, char** argv)
+{
+	int a = 10;
+
+	cout<<get_dist(0,1);
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
--- tester.cpp
+++ tester.cpp
@@ -2,66 +2,66 @@
 using namespace std;
 #define ENT cout<<"\n";
 #define IOS ios::sync_with_stdio(0);cin.tie(0);
-#define MX 10001
+#define MX 1000
+#define INF 1000000000000
 #define REP(i,a,b) for(auto i=a; i<b; i++)
 typedef vector<int> vi;
 typedef pair<int,int> pi;
 
-int v, e, t1, t2, parent[MX], child[MX][3];
+int n, x[MX], y[MX];
+double dist[MX][MX], e;
+bool visited[MX];
 
-int subtree(int n){
-	if(child[n][0] == 0) return 1; // leaf node
-
-	int ret = subtree(child[n][1]);
-	if(child[n][0] == 2) ret += subtree(child[n][2]);
-	return ret+1;
+double get_dist(int i, int j){
+	return (pow(x[i]-x[j],2) + pow(y[i]-y[j],2));
 }
 
-stack<int> find_anc(int n){
-	stack<int> ret;
-	while(parent[n]>0){
-		ret.push(parent[n]);
-		n = parent[n];
+double prime(int p, int stage){
+	if(stage == n) return 0;
+
+	visited[p] = 1;
+
+	// find nearest island
+	int v=p;
+	double d=INF;
+	REP(i,0,n){
+		if(!visited[i] && dist[p][i] < d){
+			d = dist[p][i];
+			v = i;
+		}
 	}
 
-	return ret;
-}
+	cout<<p<<"->"<<v<<": "<<(long long)dist[p][v];ENT
+	visited[v] = 1;
+	return d+prime(v, stage+1);
 
+}
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 	cin>>T;
-	parent[1] = 0;
+	cout.setf(ios::fixed);
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
-		memset(child, 0, sizeof(child));
-		cin >>v>>e>>t1>>t2;
+		memset(visited, 0, sizeof(visited));
+		cin>>n;
+		REP(i,0,n) cin>>x[i];
+		REP(i,0,n) cin>>y[i];
 
-		while(e--){
-			int p, c;
-			cin>>p>>c;
-			parent[c] = p;
-			child[p][++child[p][0]] = c;
-		}
-
-		int ancestor = 0;
-		stack<int> anc1 = find_anc(t1);
-		stack<int> anc2 = find_anc(t2);
-
-		while(!anc1.empty()){
-			if(anc1.top() != anc2.top())
-				break;
-			
-			ancestor = anc1.top();
-			anc1.pop();
-			anc2.pop();
+		REP(i,0,n){
+			REP(j,0,n){
+				if(i==j) dist[i][j] = 0;
+				else if(i>j) dist[i][j] = dist[j][i];
+				else dist[i][j] = get_dist(i,j);
+			}
 		}
 		
 
-		cout <<"#"<<test_case<<" "<<ancestor<<" "<<subtree(ancestor);
+		cout <<"#"<<test_case<<" "<<(long long) round(e*prime(0,1));
 		ENT
+
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
